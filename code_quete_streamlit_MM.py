873a595536e173dfdda9db1b60a8ac650555cf5c
MargauxMData <148775560+MargauxMData@users.noreply.github.com>
2024-01-13 18:27:36 +0100

    Delete code_quete_streamlit_MM.py

--- code_quete_streamlit_MM.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df_cars = pd.read_csv(link)
-
-print(df_cars.head())
